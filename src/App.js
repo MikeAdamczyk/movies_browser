2a8791975ddd9c51b0d48e42caca66590b12785f
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-10-07 08:50:38 +0200

    GlobalStyle and Theme into index.js/ removed .css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme"
 import "./App.css";
 import { Tile } from "./components/Tile"
 
 function App() {
   return <>
-    <ThemeProvider theme={theme}>
       <Tile></Tile>
-    </ThemeProvider>
   </>;
 }
 
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,7 @@
 export const theme = {
     color: {
         white: "#FFFFFF",
+        mercury: "#E5E5E5",
         black: "#000000",
     }
 };
