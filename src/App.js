db0e6258225ad00d4e9b981b13fdcb589baca947
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-10-07 08:32:07 +0200

    theme.js created and provided to all elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme"
 import "./App.css";
+import { Tile } from "./components/Tile"
 
 function App() {
-  return <>Test</>;
+  return <>
+    <ThemeProvider theme={theme}>
+      <Tile></Tile>
+    </ThemeProvider>
+  </>;
 }
 
 export default App;
