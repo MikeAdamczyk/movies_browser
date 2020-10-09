16653aada4180cfed72f661b635b7c794e8874ad
Aneta Szynal <anetaszynal28@gmail.com>
2020-10-09 10:51:47 +0200

    refactor(): remove idea

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {fetchMovie, selectMovie} from "./features/movies/moviesSlice";
 
 function App() {
+    //@FIXME: below is only sample render
     const moviesResult = useSelector(selectMovie)
     const dispatch = useDispatch();
     useEffect(() => {
