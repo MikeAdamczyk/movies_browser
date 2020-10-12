1e0442643ad1397070eccaebc02a87cff1664ca0
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-10-12 12:57:27 +0200

    function for getting production year created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchMovie, selectMovie} from "./features/movies/moviesSlice";
-import { Tile } from "./components/Tile"
-import poster from './images/poster.jpg'
-import posterList from './images/posterlist.png'
-import actor from './images/actor.png'
+import { Tile } from "./components/Tile";
+import poster from './images/poster.jpg';
+import posterList from './images/posterlist.png';
+import actor from './images/actor.png';
+import {Container} from './styled'
+import { store } from "./store";
 
 function App() {
     //@FIXME: below is only sample render
@@ -13,29 +15,33 @@ function App() {
     useEffect(() => {
         dispatch(fetchMovie())
     }, [])
+  
 
-    return (<>
-            {moviesResult.map((result, index) => (
-                    <div key={index}>{JSON.stringify(result)}</div>
+    const getProductionYear = (releaseDate) => {
+      const productionYear = (new Date(releaseDate)).getFullYear();
+
+      return productionYear;
+    }
+
+    return (<Container>
+            {moviesResult.map((result) => (
+                
+                  <Tile
+                    tileType={"movie"} //movie / people
+                    tileView={"list"} // list / detail
+                    header={result.title}
+                    subheader={getProductionYear(result.release_date)}
+                    poster={`https://image.tmdb.org/t/p/w342/${result.poster_path}`}
+                    place={"China, United States of America"}
+                    date={result.release_date}
+                    genres={["Adventure", "Drama", "Action"]}
+                    rateValue={result.vote_average}
+                    votesNumber={result.vote_count}
+                    description={result.overview}
+                  ></Tile> 
                 )
             )}
-                    <Tile
-                        tileType={"movie"} //movie / people
-                        tileView={"list"} // list / detail
-                        header={"Mulan"}
-                        subheader={"2020"}
-                        poster={posterList}
-                        place={"China, United States of America"}
-                        date={"24.10.2020"}
-                        genres={["Action", "Adventure", "Drama"]}
-                        rateValue={"7,8"}
-                        votesNumber={"35"}
-                        description={`A young Chinese maiden disguises herself as a male warrior in order to save her father. 
-                                      Disguises herself as a male warrior in order to save her father. 
-                                      A young Chinese maiden disguises herself as a male warrior in order to save her father.`
-                                    }
-                      ></Tile>
-        </>
+            </Container>
     )
 }
 
