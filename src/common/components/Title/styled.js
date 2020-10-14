c84673b02e917ea1770e4a74fdb8797ed47908d7
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-10-14 20:46:45 +0200

    changed Title styles a bit

--- src/common/components/Title/styled.js
+++ src/common/components/Title/styled.js
@@ -1,13 +1,8 @@
 import styled from "styled-components";
 
 export const TitleBox = styled.div`
-    max-width: 1368px;
+    max-width: 100%;
     margin: auto;
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-        margin-left: 16px;
-        margin-right: 16px;
-    }
 `;
 
 export const SectionTitle = styled.h2`
@@ -17,7 +12,10 @@ export const SectionTitle = styled.h2`
     line-height: 120%;
     margin: 56px 0 24px 0;
 
-    @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        font-size: 24px;
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
         font-size: 18px;
         line-height: 22px;
         margin: 24px 0 12px 0;
