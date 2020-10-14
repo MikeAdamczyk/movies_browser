1bb4e7488967a8bf530564d2caddbc676bb76ace
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-10-14 20:48:03 +0200

    structure changed, filesmoved to folder-common

--- src/common/components/Tile/styled.js
+++ src/common/components/Tile/styled.js
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const TileElement = styled.div`
-    max-width: 100%;
+    width: 100%;
     margin: 0 auto;
     padding: 0px 0px 0px 40px;
     background-color: ${({theme}) => theme.color.white};
@@ -18,41 +18,62 @@ export const TileElement = styled.div`
         padding: 16px;
     `}
 
+    ${({tileType}) => tileType === "people" && css`
+        padding: 10px 15px 65px 16px;
+        grid-template-rows: 264px 1fr;
+    `}
+
     @media (max-width: ${({theme}) => theme.breakpoints.mobile}){
         ${({tileView}) => tileView === "list" && css`
             grid-template-columns: auto 1fr;
             grid-template-rows: 1fr;
         `}
-    };
 
+        ${({tileType}) => tileType === "people" && css`
+            padding: 8px;
+            grid-template-columns: 1fr;
+            grid-template-rows: 178px auto;
+        `}
+    };
 `;
 
-export const Image = styled.img`
-    background-position: center;
+export const Image = styled.div`
+    background-position: top;
     background-size: cover;
+    background-repeat: no-repeat;
 
-    ${({posterPath}) =>  css`
-        background-image: url(https://image.tmdb.org/t/p/w342/${posterPath});
+    ${({imagePath, tileType}) =>  css`
+        background-image: url(https://image.tmdb.org/t/p/${tileType === "movie" ? "w342" : "w185"}/${imagePath});
     `}
     margin: 40px 0px;
     flex-grow: 1;
     border-radius: 5px;
 
     ${({tileView}) => tileView === "list" && css`
-        margin: 0px;
+        margin: 0;
         width: 292px;
         height: 434px;
     `}
+    ${({tileType}) => tileType === "people" && css`
+        width: 177px;
+        height: 249px;
+    `}
     @media (max-width: ${({theme}) => theme.breakpoints.mobile}){
-        ${({posterPath}) =>  css`
-            background-image: url(https://image.tmdb.org/t/p/w154/${posterPath});
+        ${({imagePath}) =>  css`
+            background-image: url(https://image.tmdb.org/t/p/w154/${imagePath});
         `}
 
         ${({tileView}) => tileView === "list" && css`
-        margin: 0px;
-        width: 114px;
-        height: 169px;
-    `}
+            margin: 0px;
+            width: 114px;
+            height: 169px;
+        `}
+
+        ${({tileType}) => tileType === "people" && css`
+            margin: 0px;
+            width: 120px;
+            height: 178px;
+        `}
     };
 `;
 export const DetailBox = styled.div`
@@ -80,10 +101,19 @@ export const Header = styled.h2`
         line-height: 1.3;
         margin: 0 0 0 0;
     `}
+    ${({tileType}) => tileType === "people" && css`
+        text-align: center;
+        margin-top: -15px;
+    `}
 
     @media (max-width: ${({theme}) => theme.breakpoints.mobile}){
         ${({tileView}) => tileView === "list" && css`           
             font-size: 16px;
+        `}
+
+        ${({tileType}) => tileType === "people" && css`
+            font-size: 14px;
+            margin-top: -8px;
         `}
     };
 `;
