19a674d420fa076b45ff24f7eb95601fc394b543
Aneta Szynal <anetaszynal28@gmail.com>
2020-10-15 11:27:30 +0200

    Updates

---
+++ precache-manifest.7519fc8776e33f2747cdf6e5d7151f3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d3c576cb46b4a89d4fa37bf636e791",
+    "url": "/movies_browser/index.html"
+  },
+  {
+    "revision": "5dcd88adbfea4ada474b",
+    "url": "/movies_browser/static/js/2.262be6ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies_browser/static/js/2.262be6ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b4ef4343da2b7dac70",
+    "url": "/movies_browser/static/js/main.d60614aa.chunk.js"
+  },
+  {
+    "revision": "2f39a3b712114e40cedc",
+    "url": "/movies_browser/static/js/runtime-main.07168239.js"
+  },
+  {
+    "revision": "01e4fceab2d60ea606631625788e227a",
+    "url": "/movies_browser/static/media/Vector.01e4fcea.svg"
+  },
+  {
+    "revision": "2cf439bac862fd29dd816196e850e005",
+    "url": "/movies_browser/static/media/active.2cf439ba.svg"
+  },
+  {
+    "revision": "ba0ba952f4fff89780a250f03dc5c212",
+    "url": "/movies_browser/static/media/disabled.ba0ba952.svg"
+  }
+]);
