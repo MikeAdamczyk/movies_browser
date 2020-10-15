15b299a584fc90940366cbe44fa5d15bfd4fdd30
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-10-15 11:29:09 +0200

    single page structure

--- src/features/movies/SingleMoviePage/index.js
+++ src/features/movies/SingleMoviePage/index.js
@@ -1,17 +1,33 @@
 import React from "react";
-import { Container } from "../../../common/components/Container/index";
-import Paging from "../../../common/components/Paging";
-import { Title } from "../../../common/components/Title";
+import { Footer } from "../../../common/Footer";
+import { Title } from "../../../common/Title";
+import { useMovieDetail } from "../../../lib/useMovieDetail";
 
-export const SingleMoviePage = () => (
-    <>
-        <Title title={"Singe Movie Page"}></Title>
-        <Container>
-            <p>
-                Single Movie Page
-            </p>
-        </Container>
 
-        <Paging />
-    </>
-);
+export const SingleMoviePage = () => {
+    const {getProductionYear, getMovieGenres, moviesResult} = useMovieDetail();
+    console.log(moviesResult);
+
+    return <Wrapper>
+            <Title title={"Popular movies"}></Title>
+                {moviesResult.map((result) => (
+                        <Tile
+                          key={result.id}
+                          tileType={"movie"} //movie / people
+                          tileView={"list"} // list / detail
+                          header={result.title}
+                          subheader={getProductionYear(result.release_date)}
+                          image={result.poster_path}
+                          place={"China, United States of America"}
+                          date={result.release_date}
+                          genres={getMovieGenres(result.genre_ids)}
+                          rateValue={result.vote_average}
+                          votesNumber={result.vote_count}
+                          description={result.overview}
+                        ></Tile> 
+                        )
+                    )
+                }
+            <Footer />
+        </Wrapper>
+};
