19e05615fcedc41b0b40a1c9a8ff3ef48f0d3f40
Aneta Szynal <anetaszynal28@gmail.com>
2020-10-17 19:29:33 +0200

    feat(): add page to url address

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -1,27 +1,41 @@
-import React from "react";
-import {useSelector} from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector} from "react-redux";
 import {selectCurrentPage, selectTotalPages} from "../../features/movies/moviesSlice";
+import {selectPeopleCurrentPage, selectPeopleTotalPages} from "../../features/people/peopleSlice";
 import {Pagination, Text, Number} from "./styled";
 import {BackButton} from "./BackButton";
 import {NextButton} from "./NextButton";
+import {usePageParameter} from "../../features/queryParameters";
+import {PAGE_PARAMETER} from "../../lib/consts";
 
 export const Footer = () => {
-    const currentPage = useSelector(selectCurrentPage);
-    const totalPages = useSelector(selectTotalPages);
+    const PageParameterChange = usePageParameter();
+    const moviesCurrentPage = useSelector(selectCurrentPage);
+    const moviesTotalPages = useSelector(selectTotalPages);
+    const peopleCurrentPage = useSelector(selectPeopleCurrentPage);
+    const peopleTotalPages = useSelector(selectPeopleTotalPages);
+
+    useEffect(() => {
+        PageParameterChange({
+            key: PAGE_PARAMETER,
+            value: window.location.href.includes("people") ? peopleCurrentPage : moviesCurrentPage
+        });
+    }, [moviesCurrentPage, peopleCurrentPage]);
 
     return (
         <Pagination>
             <BackButton
                 firstTitle="First"
                 secondTitle="Previous"
-                currentPage={currentPage}
             />
-            <Text>Page <Number>{currentPage}</Number> of <Number>{totalPages}</Number></Text>
+            {window.location.href.includes("people") ?
+                <Text>Page <Number>{peopleCurrentPage}</Number> of <Number>{peopleTotalPages}</Number></Text>
+                :
+                <Text>Page <Number>{moviesCurrentPage}</Number> of <Number>{moviesTotalPages}</Number></Text>
+            }
             <NextButton
                 firstTitle="Next"
                 secondTitle="Last"
-                currentPage={currentPage}
-                totalPages={totalPages}
             />
         </Pagination>
     )
