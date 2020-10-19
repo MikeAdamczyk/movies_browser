9727399c9e1e00068d2e65e600bddd72cf91b5a5
Michał Adamczyk <hello@codeman.it>
2020-10-19 12:44:44 +0200

    fix PeoplePage + add example Crew section SMP

--- src/features/movies/SingleMoviePage/index.js
+++ src/features/movies/SingleMoviePage/index.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { PeopleList } from "../../people/PeoplePage/peopleList";
+import React, { useEffect } from "react";
 import { Backdrop } from "./backdrop";
 import { DetailsContainer, ExampleTile } from "./styled";
-
+import { ListContainer, Wrapper } from "../../../common/Containers/styled";
+import { Title } from "../../../common/Title";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPeople, selectPeople } from "../../people/peopleSlice";
+import { Tile } from "../../../common/Tile";
 
 export const SingleMoviePage = () => {
+
+    const dispatch = useDispatch();
+
+    const peopleResult = useSelector(selectPeople);
+
+    useEffect(() => {
+        dispatch(fetchPeople());
+    }, [dispatch])
 
     return (
         <>
@@ -13,15 +24,25 @@ export const SingleMoviePage = () => {
             <DetailsContainer>
 
                 <ExampleTile>
-                    <p>Heloooooooo</p>
-                    <p>Siemaaaaaaaaa</p>
-                    <p>YouCode :D XD</p>
+                    Helooooooooooooo :D
                 </ExampleTile>
 
-            <PeopleList/>
+                <Wrapper DataType={"people"}>
+                    <Title title={"Crew"} />
+                    <ListContainer DataType={"people"}>
+                        {peopleResult.map((result) => (
+                            <Tile
+                                key={result.id}
+                                tileType={"people"} //movie / people
+                                tileView={"list"} // list / detail
+                                header={result.name}
+                                image={result.profile_path}
+                            />
+                        ))}
+                    </ListContainer>
+                </Wrapper>
 
             </DetailsContainer>
-
         </>
     )
 };
--- src/features/people/PeoplePage/index.js
+++ src/features/people/PeoplePage/index.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Footer } from "../../../common/Footer";
-import { PeopleList } from "./peopleList";
+import { ListContainer, Wrapper } from "../../../common/Containers/styled";
+import { Title } from "../../../common/Title";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPeople, selectPeople } from "../peopleSlice";
+import { Tile } from "../../../common/Tile";
 
-export const PeoplePage = () => (
-    <>
-        <PeopleList />
-        <Footer />
-    </>
-);
+export const PeoplePage = () => {
+
+    const dispatch = useDispatch();
+
+    const peopleResult = useSelector(selectPeople);
+
+    useEffect(() => {
+        dispatch(fetchPeople());
+    }, [dispatch])
+
+    return (
+        <Wrapper DataType={"people"}>
+            <Title title={"Popular people"} />
+            <ListContainer DataType={"people"}>
+                {peopleResult.map((result) => (
+                    <Tile
+                        key={result.id}
+                        tileType={"people"} //movie / people
+                        tileView={"list"} // list / detail
+                        header={result.name}
+                        image={result.profile_path}
+                    />
+                ))}
+            </ListContainer>
+            <Footer />
+        </Wrapper>
+    )
+};
