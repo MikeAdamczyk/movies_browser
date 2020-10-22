cfaeb65b6e2a07b01168787a0f2904b6323a8d44
Aneta Szynal <anetaszynal28@gmail.com>
2020-10-22 20:13:09 +0200

    fix(): add search ability on the details page

--- src/features/movies/MovieCredits/movieCreditsSlice.js
+++ src/features/movies/MovieCredits/movieCreditsSlice.js
@@ -1,36 +1,42 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 export const movieCreditsSlice = createSlice({
-    name: "movieCredits",
-    initialState: {
-        isLoading: false,
-        movie_id: undefined,
-        cast: [],
-        crew: [],
+  name: "movieCredits",
+  initialState: {
+    isLoading: false,
+    isError: false,
+    movie_id: undefined,
+    cast: [],
+    crew: [],
+  },
+  reducers: {
+    fetchMovieCredits: (state, {payload}) => {
+      state.isLoading = true;
     },
-    reducers: {
-        fetchMovieCredits: (state, {payload}) => {
-            state.isLoading = true;
-        },
-        fetchMovieCreditsSuccess: (state, {payload}) => {
-            state.isLoading = false;
-            state.movie_id = payload.id;
-            state.cast = payload.cast;
-            state.crew = payload.crew;
-        },
-        fetchMovieCreditsError: (state) => {
-            state.isLoading = false;
-        },
+    fetchMovieCreditsSuccess: (state, {payload}) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.movie_id = payload.id;
+      state.cast = payload.cast;
+      state.crew = payload.crew;
     },
+    fetchMovieCreditsError: (state) => {
+      state.isLoading = false;
+      state.isError = true;
+    },
+  },
 });
 
 export const {
-    fetchMovieCredits,
-    fetchMovieCreditsSuccess,
-    fetchMovieCreditsError,
+  fetchMovieCredits,
+  fetchMovieCreditsSuccess,
+  fetchMovieCreditsError,
 } = movieCreditsSlice.actions;
 
 export const selectMovieCast = (state) => state.movieCredits.cast;
 export const selectMovieCrew = (state) => state.movieCredits.crew;
+export const selectIsMovieCreditsLoading = (state) => state.movieCredits.isLoading;
+export const selectIsMovieCreditsError = (state) => state.movieCredits.isError;
+
 
 export default movieCreditsSlice.reducer;
