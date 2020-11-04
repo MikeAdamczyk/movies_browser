46a306bdccbfd6a95468d4f2e3fa1437ccc3e108
Aneta Szynal <anetaszynal28@gmail.com>
2020-11-04 11:51:44 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movies_browser/precache-manifest.7505cd6e6fa895a726917a8d7b4afa33.js"
+  "/movies_browser/precache-manifest.5810e5d63bb1ee4c940c2a452bfde267.js"
 );
 
 self.addEventListener('message', (event) => {
